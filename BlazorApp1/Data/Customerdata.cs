6fbf2db96922ec3d3dbb3e5f66a9095fbcf88046
adnanyangilic <adnan.alistika@gmail.com>
2022-04-05 12:51:12 +0300

    Proje dosyası ekle.

---
+++ BlazorApp1/Data/Customerdata.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Customerdata
+    {
+
+
+        public int id { get; set; }
+        public int userId { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+}
